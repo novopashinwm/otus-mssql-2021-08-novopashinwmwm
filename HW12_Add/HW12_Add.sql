cd0844ed0ef2346ceef60d76b48bbb9bf5de1281
Vladimir Novopashin <novopashinwm@mail.ru>
2021-10-13 18:51:05 +0300

    Make HW12_Add - GetIDClientMaxSum

---
+++ HW12_Add/HW12_Add.sql
@@ -0,0 +1,18 @@
+﻿use WideWorldImporters;
+go
+/*Написать хранимую процедуру возвращающую Клиента с набольшей разовой суммой покупки.*/
+CREATE procedure Sales.GetCustomerIdMaxSumma
+WITH EXECUTE AS OWNER
+AS
+BEGIN
+
+	SELECT TOP 1	Sales.Customers.CustomerID
+		FROM		Sales.Invoices 
+		            join Sales.InvoiceLines ON Sales.Invoices.InvoiceID = Sales.InvoiceLines.InvoiceID 
+					join Sales.Customers ON Sales.Invoices.CustomerID = Sales.Customers.CustomerID
+		GROUP BY Sales.Customers.CustomerID, Sales.Customers.CustomerName
+	ORDER BY SUM(Sales.InvoiceLines.Quantity*Sales.InvoiceLines.UnitPrice) DESC
+END
+GO
+exec Sales.GetCustomerIdMaxSumma
+go
