6cd7c9a36c160afb047136173916aa00e2325e13
Vladimir Novopashin <novopashinwm@mail.ru>
2021-10-16 21:20:02 +0300

    Fixing aggregate function

--- HW13/CLRNovopashinWM/CLRNovopashinWM/03 Aggregate/MySTRING_AGG.sql
+++ HW13/CLRNovopashinWM/CLRNovopashinWM/03 Aggregate/MySTRING_AGG.sql
@@ -1,10 +1,12 @@
 ﻿use WideWorldImporters
-/*DROP ASSEMBLY IF EXISTS DemoNovopashinWM
-GO
-CREATE ASSEMBLY DemoNovopashinWM
-FROM 'D:\projects\OTUS\SQL\otus-mssql-2021-08-novopashinwmwm\HW13\CLRNovopashinWM\CLRNovopashinWM\bin\Debug\CLRNovopashinWM.dll'
-WITH PERMISSION_SET = SAFE; */
 go
 CREATE AGGREGATE MySTRING_AGG (@input nvarchar(200), @delimiter nvarchar(10)) RETURNS nvarchar(max)  
 EXTERNAL NAME DemoNovopashinWM.[CLRNovopashinWM.MySTRING_AGG];
+go
+declare @table table (n1 varchar(255))
+insert @table values ('first'),('second'),('third'),('four')
+select * from @table
+
+select STRING_AGG(n1,',') from @table
+select dbo.MySTRING_AGG(n1,',') from @table
 go
