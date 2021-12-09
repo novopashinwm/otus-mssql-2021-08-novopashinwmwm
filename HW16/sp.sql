af82119e456b71710f16931b85dfde3f285a026b
Novopashin Vladimir <vnovopashin@vtb.ru>
2021-12-09 13:31:05 +0300

    Add out docs sp

--- HW16/sp.sql
+++ HW16/sp.sql
@@ -19,6 +19,7 @@ create or alter procedure spAddDoc
 as
 begin
     insert into Docs values (@UserID, GetDate(), null, null)
+	select SCOPE_IDENTITY() as ID
 end
 go
 create or alter procedure spAddDocInBook
@@ -36,6 +37,28 @@ begin
 	exec spAddSaldo @BookID, @Saldo, @Qty, 0
 end
 go
+
+create or alter procedure spAddDocOutBook
+(
+     @DocID bigint
+   , @BookID bigint
+   , @Qty int
+   , @ReaderID bigint
+   , @DateReturn datetime2
+)
+as
+begin
+
+	insert into DocsOut values (@DocID, @BookID, @Qty, @ReaderID, @DateReturn)
+	declare @Saldo int = Isnull((select top 1 SaldoEnd from BooksSaldo where BookID = @BookID order by BookSaldoID desc),0)
+	exec spAddSaldo @BookID, @Saldo, 0, @Qty
+end
+go
 exec spAddDoc @UserID=1
 go
 exec spAddDocInBook @DocID= 1, @CupboardID=2, @BookID= 4, @Qty= 7
+go
+exec spAddDoc @UserID=2
+go
+exec spAddDocOutBook @DocID= 2, @BookID= 4, @Qty= 1, @ReaderID =2, @DateReturn ='2021-12-21'
+go
